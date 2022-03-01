eb9420d3816a51b694e185819dbfea6f5058389e
“chengxulee” <1695172472@qq.comgit config --global user.email 1695172472@qq.com>
2022-03-01 12:54:16 +0800

    .

---
+++ leetcode/树/leetcode 100. 相同的树.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+using namespace std;
+//
+//给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+//
+//如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) return true;
+        if (p == nullptr || q == nullptr) return false;
+        if (p->val != q->val) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+
+int main() {
+
+	system("pause");
+	return 0;
+}
