8d93b477d916f514d88046a4b39f25372e07d9ec
“chengxulee” <1695172472@qq.comgit config --global user.email 1695172472@qq.com>
2022-03-02 11:50:52 +0800

    .

---
+++ leetcode/链表/leetcode 21.  合并两个有序链表.cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+using namespace std;
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* list3 = new ListNode(0);
+        ListNode* p = list3;
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val <= list2->val) {
+                p->next = list1;
+                p = p->next;
+                list1 = list1->next;
+            }
+            else {
+                p->next = list2;
+                p = p->next;
+                list2 = list2->next;
+            }
+
+        }
+        if (list1 == nullptr) {
+            p->next = list2;
+        }
+        else if (list2 == nullptr) {
+            p->next = list1;
+        }
+        return list3->next;
+    }
+};
+int main() {
+
+
+	system("pause");
+	return 0;
+}
---
+++ leetcode/链表/leetcode 83. 删除排序链表中的重复元素.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+using namespace std;
+//给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+//输入：head = [1, 1, 2, 3, 3]
+//输出：[1, 2, 3]
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return head;
+        ListNode* p = head;
+        ListNode* p1 = head->next;
+        while (p1 != nullptr) {
+            if (p1->val == p->val) {
+                p1 = p1->next;
+                p->next = p1;
+            }
+            else {
+                p = p1;
+                p1 = p1->next;
+            }
+        }
+        return head;
+    }
+};
+int main() {
+
+
+	system("pause");
+	return 0;
+}
