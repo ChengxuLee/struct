225e709184c1ce173099775e922d90abe5343320
“chengxulee” <1695172472@qq.comgit config --global user.email 1695172472@qq.com>
2022-03-05 13:55:41 +0800

    .

---
+++ leetcode/链表/leetcode  86. 分隔链表.cpp
@@ -0,0 +1,47 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+using namespace std;
+//给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。
+//
+//你应当 保留 两个分区中每个节点的初始相对位置。
+//输入：head = [1, 4, 3, 2, 5, 2], x = 3
+//输出：[1, 2, 2, 4, 3, 5]
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        if (head == nullptr || head->next == nullptr) return head;
+        ListNode* l1 = new ListNode();
+        ListNode* ll1 = l1;
+        ListNode* l2 = new ListNode();
+        ListNode* ll2 = l2;
+        ListNode* ptr = head;
+        while (ptr != nullptr) {
+            if (ptr->val < x) {
+                l1->next = ptr;
+                l1 = ptr;
+            }
+            else {
+                l2->next = ptr;
+                l2 = ptr;
+            }
+            ptr = ptr->next;
+        }
+        if (ll1->next == nullptr) {
+            return ll2->next;
+        }
+        else if (ll2->next == nullptr) {
+            return ll1->next;
+        }
+        else {
+            l1->next = ll2->next;
+            l2->next = nullptr;
+        }
+        return ll1->next;
+    }
+};
+int main() {
+
+
+	system("pause");
+	return 0;
+}
---
+++ leetcode/链表/leetcode 61. 旋转链表.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+using namespace std;
+//给你一个链表的头节点 head ，旋转链表，将链表每个节点向右移动 k 个位置。
+//输入：head = [1, 2, 3, 4, 5], k = 2
+//输出：[4, 5, 1, 2, 3]
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (head == nullptr || head->next == nullptr || k == 0) return head;
+        //环形链表解决  先连接再断开 
+        int len = 1;
+        ListNode* p = head;
+        while (p->next != nullptr) {
+            len++;    //计算总长度
+            p = p->next;  //p移动
+        }
+        p->next = head;//形成环
+        //断开连接  连接的位置是 (len-k)%len 
+        k = k % len;
+        int count = (len - k) % len;
+
+        while (count > 0) {
+            p = p->next;
+            count--;
+        }
+        ListNode* newhead = p->next;
+        p->next = nullptr;
+        return newhead;
+    }
+};
+int main() {
+
+
+	system("pause");
+	return 0;
+}
